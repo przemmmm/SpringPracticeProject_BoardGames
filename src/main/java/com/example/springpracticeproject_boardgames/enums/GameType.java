02853793757987c1eb721e08ea929be11cfaf983
Patryk Kusz <koszu024@gmail.com>
2023-05-14 16:09:59 +0200

    Commit 4. Display products added.

--- src/main/java/com/example/springpracticeproject_boardgames/enums/GameType.java
+++ src/main/java/com/example/springpracticeproject_boardgames/enums/GameType.java
@@ -1,5 +1,9 @@
 package com.example.springpracticeproject_boardgames.enums;
 
 public enum GameType {
-    RPG, STRATEGY, FAMILY, ADVENTURE, CARD, ADULT, SOCIAL
+    RPG, STRATEGY, FAMILY, ADVENTURE, CARD, ADULT, SOCIAL;
+
+    GameType() {
+    }
 }
+
