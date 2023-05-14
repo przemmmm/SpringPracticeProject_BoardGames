2f1e26e08a3b2fdb671bb2b731155372e461cf58
Przemek Koscinski <przem.koscinski@gmail.com>
2023-05-14 12:25:16 +0200

    Commit 1 - entity package created

---
+++ src/main/java/entity/BoardGames.java
@@ -0,0 +1,20 @@
+package entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table(name = "board games" )
+public class BoardGames {
+
+
+}
