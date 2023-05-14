d3ef941e9d573ec363c36ba66fa5da2081766ac7
Przemek Koscinski <przem.koscinski@gmail.com>
2023-05-14 12:00:28 +0200

    Initial commit

---
+++ src/main/java/com/example/springpracticeproject_boardgames/SpringPracticeProjectBoardGamesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeproject_boardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeProjectBoardGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeProjectBoardGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springpracticeproject_boardgames/SpringPracticeProjectBoardGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeproject_boardgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeProjectBoardGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
